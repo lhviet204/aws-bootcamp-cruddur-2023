d3f735381e80a684eafea8ad9577bc5cda477cb6
Lu <mrlu204@gmail.com>
2023-03-04 16:12:37 +0700

    feat: add span for trace

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -20,13 +20,17 @@ from opentelemetry.instrumentation.requests import RequestsInstrumentor
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
-
+from opentelemetry.sdk.trace.export import ConsoleSpanProcessor, SimpleSpanProcessors
 # Initialize tracing and an exporter that can send data to Honeycomb
 provider = TracerProvider()
 processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
+
+# to send logs to console on BE flask by STDOUT 
+simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
+provider.add_span_processor(simple_processor)
 
 # Initialize automatic instrumentation with Flask
 app = Flask(__name__)

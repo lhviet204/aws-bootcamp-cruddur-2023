8b7e743ceeb12e1b9fe581eaa2fe7a7093cac432
Lu <mrlu204@gmail.com>
2023-03-11 19:22:38 +0700

    feat: init Cognito

--- frontend-react-js/src/App.js
+++ frontend-react-js/src/App.js
@@ -15,6 +15,23 @@ import {
   RouterProvider
 } from "react-router-dom";
 
+import { Amplify } from 'aws-amplify';
+
+Amplify.configure({
+  "AWS_PROJECT_REGION": process.env.REACT_APP_AWS_PROJECT_REGION,
+  "AWS_COGNITO_REGION": process.env.REACT_APP_AWS_COGNITO_REGION,
+  "AWS_USER_POOLS_ID": process.env.REACT_APP_AWS_USER_POOLS_ID,
+  "AWS_USER_POOLS_WEB_CLIENT_ID": process.env.REACT_APP_CLIENT_ID,
+  "oauth": {},
+  Auth: {
+    // We are not using an Identity Pool
+    // identityPoolId: process.env.REACT_APP_IDENTITY_POOL_ID, // REQUIRED - Amazon Cognito Identity Pool ID
+    region: process.env.REACT_APP_AWS_PROJECT_REGION,           // REQUIRED - Amazon Cognito Region
+    userPoolId: process.env.REACT_APP_AWS_USER_POOLS_ID,         // OPTIONAL - Amazon Cognito User Pool ID
+    userPoolWebClientId: process.env.REACT_APP_CLIENT_ID,   // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+  }
+});
+
 const router = createBrowserRouter([
   {
     path: "/",
--- journal/week3.md
+++ journal/week3.md
@@ -1,1 +1,25 @@
 # Week 3 — Decentralized Authentication
+- [Summary](#summary)
+- [Homework](#homework)
+- [Homework Challenges](#homework-challenges)
+
+## Summary
+## Homework
+## Homework Challenges
+## Try Harder
+
+
+- Commit [1d26acf](https://github.com/lhviet204/aws-bootcamp-cruddur-2023/commit/1d26acf5fef57905e8c1aa231279abbe89ad3284)
+
+![](./assets/week2/)
+
+
+## Steps
+- Configure Cognito on AWS
+- Install the lib from frontend-react-js
+    npm i aws-amplify --save
+- Import lib and to configure Auth on app.js
+- Edit DesktopSideBar
+- Edit ProfileInfo.js to remove user session data after signing out
+- Edit HomeFeedPage.js to show display name and preffered_username
+- Edit SignInPage.js to set JWT toke to browsers' local storage of users.

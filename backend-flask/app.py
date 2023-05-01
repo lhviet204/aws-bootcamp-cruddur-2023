8f4adf6f86afa598413c6482f72bb765838316a9
lhviet204 <mrlu204@gmail.com>
2023-05-01 14:59:32 +0000

    fix: fixing typo

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -91,17 +91,17 @@ cors = CORS(
 )
 
 
-rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+# rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
 
-@app.before_first_request
-def init_rollbar():
-    """init rollbar module"""
-    rollbar.init(rollbar_access_token,
-        'production',
-        root=os.path.dirname(os.path.realpath(__file__)),
-        allow_logging_basic_config=False)
-    # send exceptions from `app` to rollbar, using flask's signal system.
-    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+# @app.before_first_request
+# def init_rollbar():
+#     """init rollbar module"""
+#     rollbar.init(rollbar_access_token,
+#         'production',
+#         root=os.path.dirname(os.path.realpath(__file__)),
+#         allow_logging_basic_config=False)
+#     # send exceptions from `app` to rollbar, using flask's signal system.
+#     got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
 
 # #Logger to log any after
 # @app.after_request
